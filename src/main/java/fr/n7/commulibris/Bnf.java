c5a65e8e4593fc5f6e13b13598512f1edd57124b
Elio <control.juin@gmail.com>
2021-05-26 14:47:22 +0200

    feat: jena for sparql queries

--- src/main/java/fr/n7/commulibris/Bnf.java
+++ src/main/java/fr/n7/commulibris/Bnf.java
@@ -1,10 +1,33 @@
 package fr.n7.commulibris;
 
+import org.apache.jena.query.ParameterizedSparqlString;
+
 /**
  * Classe gérant la communication avec l'API de la BNF.
  * @author Elio Saboureau
  * @version 1.0
  */
 public class Bnf {
-    
+
+    /**
+     * Constructeur de la classe.
+     */
+    public Bnf() {}
+
+    /**
+     * Rechercher un livre par son auteur.
+     * @param auteur auteur
+     */
+    public void searchForAuteur(String auteur) {
+        ParameterizedSparqlString p = new ParameterizedSparqlString();
+    }
+
+    /**
+     * Rechercher un livre par son nom
+     * @param nom nom
+     */
+    public void saerchForNom(String nom) {
+        ParameterizedSparqlString p = new ParameterizedSparqlString();
+    }
+
 }
